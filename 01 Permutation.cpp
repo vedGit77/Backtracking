2ce02106ea34cac4369ca1d55a8e4023c512691f
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-07 10:37:29 +0530

    Update 01 Permutation.cpp

--- 01 Permutation.cpp
+++ 01 Permutation.cpp
@@ -11,7 +11,7 @@ void permute(string str, int l, int r)
 {
     if(l==r)
     {
-        if(str.find("AB")==string::npos)
+        if(str.find("AB")==string::npos) //check for each permutation if "AB" is NOT a sub-string
             cout<<str<<" ";
         return;
     }
@@ -19,9 +19,9 @@ void permute(string str, int l, int r)
     {
         for(int i=l;i<=r;i++)
 	{
-            swap(str[i],str[l]);
-            permute(str,l+1,r);
-            swap(str[i],str[l]);
+            swap(str[i],str[l]); //swap l and i
+            permute(str,l+1,r); //recursion 
+            swap(str[i],str[l]); //swap again to nullify the effect of previous swap...this is similar to backtracking...1st push in vector, then after recursive call, pop from vector
         }   
     }
 }
@@ -40,9 +40,9 @@ int main() {
 using namespace std;
 
 bool isSafe(string str,int l, int i, int r){
-    if(l!=0 && str[l-1]=='A' && str[i]=='B')
+    if(l!=0 && str[l-1]=='A' && str[i]=='B') //obvio...since i and l are swapped
         return false;
-    if(r==(l+1) && str[i]=='A' && str[l]=='B')
+    if(r==(l+1) && str[i]=='A' && str[l]=='B') // r==(l+1)...IMP->eg string is CBA...first B swapped with B, gives CBA as output, then B swapped with A...we get CAB...has AB sub-string, BUT above code DOES NOT handle it...so we need this extra line
         return false;
     return true;
 }
@@ -57,7 +57,7 @@ void permute(string str, int l, int r){
     {
         for(int i=l;i<=r;i++)
 	{
-            if(isSafe(str,l,i,r))
+            if(isSafe(str,l,i,r)) //extra check function added...if this is true, then only swap and recursion
 	    {
                 swap(str[i],str[l]);
                 permute(str,l+1,r);
